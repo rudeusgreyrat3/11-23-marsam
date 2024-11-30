9e8fb72a9b6df81680327961484acfd7bc9b3806
rudeusgreyrat3 <samigullinmarat58@gmail.com>
2024-11-30 08:37:49 +0300

    Add files via upload

---
+++ sm4/js/modal.js
@@ -0,0 +1,12 @@
+function OpenModal(){
+    const modal = document.querySelector('.modal');
+    modal.classList.add('open');
+    console.log(modal);
+}
+
+
+
+function closeModal(){
+    const modal = document.querySelector(".modal");
+    modal.classList.remove('open');
+}
